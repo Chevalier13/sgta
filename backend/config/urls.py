92f9a73868a0a711b22f675d6c28411ffb748cbe
victor <victordevpbi@gmail.com>
2026-04-20 20:47:57 -0300

    Entrega Final: App Usuarios, Tarefas e Rotas corrigidas

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tarefas.urls')),
-]
+    path('tarefas/', include('tarefas.urls')),
+    path('usuarios/', include('usuarios.urls')),
+] 
